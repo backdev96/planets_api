047d0a08f2531c5c6c33f407eb48fe626755035f
Stas <stasefremovx@gmail.com>
2022-09-05 23:36:30 +0200

    code from example

--- src/web/project/config.py
+++ src/web/project/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
-    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
+    STATIC_FOLDER = f"{os.environ.get('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{os.environ.get('APP_FOLDER')}/project/media"
